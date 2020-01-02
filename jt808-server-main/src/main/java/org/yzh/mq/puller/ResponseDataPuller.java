cd020f264d2ab8bc314c8c8aeb95b8538c28d072
ligl <ligl@avic-digital.com/avicit>
2020-01-02 16:55:55 +0800

    初步接入redis2

--- jt808-server-main/src/main/java/org/yzh/mq/puller/ResponseDataPuller.java
+++ jt808-server-main/src/main/java/org/yzh/mq/puller/ResponseDataPuller.java
@@ -2,7 +2,7 @@ package org.yzh.mq.puller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -10,7 +10,7 @@ import org.springframework.util.StringUtils;
 public class ResponseDataPuller {
 
     @Autowired
-    RedisTemplate redisTemplate;
+    StringRedisTemplate stringRedisTemplate;
 
     @Value("${redis.key.queue.response}")
     String redisKey;
@@ -25,15 +25,22 @@ public class ResponseDataPuller {
         new Thread(new Runnable() {
             @Override
             public void run() {
+//                for (int i = 0; i < 10; i++) {
+//                    Register register = new Register();
+//                    register.setCityId(23);
+//                    register.setLicensePlate("xdssd我们");
+//                    stringRedisTemplate.opsForList().rightPush(redisKey, JsonUtils.toJson(register));
+//                }
                 try {
                     while (true) {
-                        Object data = redisTemplate.opsForList().leftPop(redisKey);
+                        String data = stringRedisTemplate.opsForList().leftPop(redisKey);
                         if (null == data) {
                             // Sleep 3秒
                             Thread.sleep(3000);
-                            System.out.println("### no response data. sleep 3 seconds.");
+                            System.out.println("### no response data. sleep 3 seconds." + System.currentTimeMillis());
                         } else {
-                            System.out.println("### got one data.");
+                            System.out.println("### got one data." + System.currentTimeMillis());
+
                         }
                     }
                 } catch (InterruptedException e) {
