e74d4784de6fb65302fe1a8d7c4ecc5136fc98e2
ligl <ligl@avic-digital.com/avicit>
2020-01-13 15:48:52 +0800

    udp适配改造初步完成，已添加配置文件

--- ant-msger-main/src/main/java/com/ant/msger/main/web/config/NettyConfig.java
+++ ant-msger-main/src/main/java/com/ant/msger/main/web/config/NettyConfig.java
@@ -2,6 +2,7 @@ package com.ant.msger.main.web.config;
 
 import com.ant.msger.main.framework.mapping.HandlerMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.ant.msger.main.framework.UDPServer;
@@ -13,13 +14,27 @@ public class NettyConfig {
     @Value("${system.session.minutes:5}")
     private int sessionMinutes;
 
+    @Value("${system.udp.port:7612}")
+    private int udpPort;
+
+    @Value("${system.tcp.port:7613}")
+    private int tcpPort;
+
     @Bean
+    @ConditionalOnExpression("${system.udp.enable:true}")
     public UDPServer udpServer() {
-//        TCPServer server = new TCPServer(7611, (byte) 0x7e, handlerMapper());
-        UDPServer server = new UDPServer(7612, (byte) 0x7e, handlerMapper(), sessionMinutes);
+        UDPServer server = new UDPServer(udpPort, (byte) 0x7e, handlerMapper(), sessionMinutes);
         server.startServer();
         return server;
     }
+
+//    @Bean
+//    @ConditionalOnExpression("${system.tcp.enable:false}")
+//    public UDPServer tcpServer() {
+//        UDPServer server = new UDPServer(udpPort, (byte) 0x7e, handlerMapper(), sessionMinutes);
+//        server.startServer();
+//        return server;
+//    }
 
     @Bean
     public HandlerMapper handlerMapper() {
