1cd850de445e175f68644a2652952a04d4238eb6
ligl <ligl@avic-digital.com/avicit>
2020-02-18 15:47:49 +0800

    修改为tomcat启动

--- ant-msger-main/src/main/java/com/ant/msger/main/framework/handler/WebSocketPrefixHandler.java
+++ ant-msger-main/src/main/java/com/ant/msger/main/framework/handler/WebSocketPrefixHandler.java
@@ -2,42 +2,40 @@ package com.ant.msger.main.framework.handler;
 
 import com.ant.msger.main.framework.commons.transform.HexUtil;
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.EmptyByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import io.netty.handler.codec.http.websocketx.*;
 import io.netty.util.CharsetUtil;
-import org.apache.log4j.Logger;
-
-import java.nio.ByteBuffer;
-import java.nio.charset.Charset;
-import java.util.Date;
 
 import static io.netty.handler.codec.http.HttpUtil.isKeepAlive;
 
+//import org.apache.log4j.Logger;
+
 public class WebSocketPrefixHandler extends ChannelInboundHandlerAdapter {
 
-    private final Logger logger= Logger.getLogger(this.getClass());
+//    private final Logger logger= Logger.getLogger(this.getClass());
 
     private WebSocketServerHandshaker handshaker;
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        if (msg instanceof FullHttpRequest){
+        if (msg instanceof FullHttpRequest) {
             //以http请求形式接入，但是走的是websocket
             handleHttpRequest(ctx, (FullHttpRequest) msg);
-        }else if (msg instanceof WebSocketFrame){
+        } else if (msg instanceof WebSocketFrame) {
             //处理websocket客户端的消息
             handlerWebSocketFrame(ctx, (WebSocketFrame) msg);
         }
     }
 
-    private void handlerWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame){
+    private void handlerWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
         // 判断是否关闭链路的指令
         if (frame instanceof CloseWebSocketFrame) {
             handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
@@ -61,9 +59,10 @@ public class WebSocketPrefixHandler extends ChannelInboundHandlerAdapter {
 //        byteBuf.writeBytes(text.getBytes());
         ctx.fireChannelRead(byteBuf);
     }
+
     /**
      * 唯一的一次http请求，用于创建websocket
-     * */
+     */
     private void handleHttpRequest(ChannelHandlerContext ctx,
                                    FullHttpRequest req) {
         //要求Upgrade为websocket，过滤掉get/Post
@@ -84,9 +83,10 @@ public class WebSocketPrefixHandler extends ChannelInboundHandlerAdapter {
             handshaker.handshake(ctx.channel(), req);
         }
     }
+
     /**
      * 拒绝不合法的请求，并返回错误信息
-     * */
+     */
     private static void sendHttpResponse(ChannelHandlerContext ctx,
                                          FullHttpRequest req, DefaultFullHttpResponse res) {
         // 返回应答给客户端
--- ant-msger-main/src/main/java/com/ant/msger/main/web/Application.java
+++ ant-msger-main/src/main/java/com/ant/msger/main/web/Application.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
@@ -12,9 +14,15 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 @SpringBootApplication
 @ComponentScan({"com.ant.msger"})
 @MapperScan("com.ant.msger.main.persistence.dao")
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
